1b066d8cd11af3c92561543bd0a5f5e4363a6e32
twosee <twose@qq.com>
2018-06-28 21:56:20 +0800

    Add file download example.

---
+++ examples/download.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: Twosee <twose@qq.com>
+ * Date: 2018/6/28 下午8:48
+ */
+
+use Swlib\SaberGM;
+
+require '../vendor/autoload.php';
+
+go(function () {
+    $download_dir = '/tmp/saber.jpg';
+    $response = SaberGM::download(
+        'https://ws1.sinaimg.cn/large/006DQdzWly1fsr8jt2botj31hc0wxqfs.jpg',
+        $download_dir
+    );
+    if ($response->success) {
+        exec('open ' . $download_dir);
+    }
+});
