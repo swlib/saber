5adbb0579f2fef23cef5260e29bd0bc5586af519
twosee <twose@qq.com>
2018-07-08 22:34:53 +0800

    Add IDE-helper Exception.

---
+++ src/include/ide-helper.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Author: Twosee <twose@qq.com>
+ * Date: 2018/7/8 下午3:00
+ */
+
+namespace Swlib\Http\Exception {
+
+    use Swlib\Saber\Request;
+    use Swlib\Saber\Response;
+
+    class TransferException extends \RuntimeException
+    {
+    }
+
+    /**
+     * Class RequestException
+     * @package Swlib\Http\Exception
+     * @method Request getRequest()
+     * @method Response|null getResponse()
+     */
+    class RequestException extends TransferException
+    {
+    }
+
+    class BadResponseException extends RequestException
+    {
+    }
+
+    class ClientException extends RequestException
+    {
+    }
+
+    class ConnectException extends RequestException
+    {
+    }
+
+    class ServerException extends RequestException
+    {
+    }
+
+    class TooManyRedirectsException extends RequestException
+    {
+    }
+
+}
