bb3fdc8996cc1834a4a20fc4984871565407206c
blcat <bl_cat@163.com>
2018-07-14 23:10:21 +0800

    add Random UserAgent

---
+++ ext/RandomUA/UserAgent.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blcat <bl_cat@163.com>
+ * Date: 2018/7/14
+ * Time: 22:38
+ */
+
+namespace Swlib\saber;
+
+class UserAgent
+{
+    static public function getUserAgent():string
+    {
+        $filename = "user-agents.txt";
+        if(file_exists($filename)){
+            swoole_async_readfile($filename, function ($filename, $content){
+                return self::afterget($content);
+            });
+        }else {
+            $uri = "https://raw.githubusercontent.com/sqlmapproject/sqlmap/master/txt/user-agents.txt";
+            $UA = SaberGM::get($uri)->body;
+            swoole_async_writefile($filename, $UA);
+            return self::afterget($UA);
+        }
+    }
+
+    public function afterget(string $UA):string
+    {
+        $ua_arr = explode(PHP_EOL, $UA);
+        // 随机获取一行作为ua
+        $ua = $ua_arr[mt_rand(0,count($ua_arr))];
+        while (preg_match('[# ]', $ua)){
+            $ua = $ua_arr[mt_rand(0,count($ua_arr))];
+        }
+        return $ua;
+    }
+}
